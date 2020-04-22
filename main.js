4269303d17169b1a76b6215fe06f032e8a095b8a
Gigi1111 <airolg1111@gmail.com>
2020-04-22 15:43:27 +0200

    [Story-003/004] create map UI so user can see what offer/need is in their proximity

--- main.js
+++ main.js
@@ -1,43 +1,48 @@
 const port = 3000,
-  http = require("http"),
-  httpStatus = require("http-status-codes"),
-  router = require("./router"),
-  contentTypes = require("./contentTypes"),
-  utils = require("./utils");
+    http = require("http"),
+    httpStatus = require("http-status-codes"),
+    router = require("./router"),
+    contentTypes = require("./contentTypes"),
+    utils = require("./utils");
 
 router.get("/", (req, res) => {
-  res.writeHead(httpStatus.OK, contentTypes.htm);
-  utils.getFile("views/index.html", res);
+    res.writeHead(httpStatus.OK, contentTypes.htm);
+    utils.getFile("views/index.html", res);
 });
 
 router.get("/volunteer", (req, res) => {
-  res.writeHead(httpStatus.OK, contentTypes.html);
-  utils.getFile("views/volunteer.html", res);
+    res.writeHead(httpStatus.OK, contentTypes.html);
+    utils.getFile("views/volunteer.html", res);
 });
 
 router.get("/requester", (req, res) => {
-  res.writeHead(httpStatus.OK, contentTypes.html);
-  utils.getFile("views/requester.html", res);
+    res.writeHead(httpStatus.OK, contentTypes.html);
+    utils.getFile("views/requester.html", res);
+});
+
+router.get("/locate", (req, res) => {
+    res.writeHead(httpStatus.OK, contentTypes.html);
+    utils.getFile("views/locate.html", res);
 });
 
 router.get("/kiezhelp.css", (req, res) => {
-  res.writeHead(httpStatus.OK, contentTypes.css);
-  utils.getFile("public/css/kiezhelp.css", res);
+    res.writeHead(httpStatus.OK, contentTypes.css);
+    utils.getFile("public/css/kiezhelp.css", res);
 });
 
 router.get("/w3.css", (req, res) => {
-  res.writeHead(httpStatus.OK, contentTypes.css);
-  utils.getFile("public/css/w3.css", res);
+    res.writeHead(httpStatus.OK, contentTypes.css);
+    utils.getFile("public/css/w3.css", res);
 });
 
 router.get("/contact.html", (req, res) => {
-  res.writeHead(httpStatus.OK, contentTypes.html);
-  utils.getFile("views/contact.html", res);
+    res.writeHead(httpStatus.OK, contentTypes.html);
+    utils.getFile("views/contact.html", res);
 });
 
 router.get("kiezhelp_bg.jpg", (req, res) => {
-  res.writeHead(httpStatus.OK, contentTypes.jpg);
-  utils.getFile("public/images/kiezhelp_bg.jpg", res);
+    res.writeHead(httpStatus.OK, contentTypes.jpg);
+    utils.getFile("public/images/kiezhelp_bg.jpg", res);
 });
 
 http.createServer(router.handle).listen(port);
