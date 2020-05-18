73d7a74e6b6e269a23c399913b8c59493f2415ed
rucsteady <rucsteady@gmx.de>
2020-05-18 10:46:37 +0200

    added mongo db connection in main

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ const express = require("express"),
     layouts = require("express-ejs-layouts");
 
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/kiezhelp", {
-    useNewUrlParser: true,
+mongoose.connect(process.env.MONGODB_URI || "mongodb://kiezhelp1:kiezhelp1@ds261479.mlab.com:61479/heroku_w4qr4v2r", {
+  useNewUrlParser: true,
 });
 const db = mongoose.connection;
 db.once("open", () => {
